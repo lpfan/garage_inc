7d81c45864a7b4a04bc468362eeb9b4678b27370
rudy <misha.shchetinin@gmail.com>
2018-01-28 20:05:01 +0200

    Added handler for climate

--- be/src/api/main.go
+++ be/src/api/main.go
@@ -7,6 +7,7 @@ import (
 	"net/http"
 	"os"
 
+	"github.com/d2r2/go-dht"
 	"github.com/gorilla/mux"
 	"github.com/stianeikeland/go-rpio"
 )
@@ -26,7 +27,17 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/v1/light/turnlamp", getLamp).Methods("GET")
 	router.HandleFunc("/api/v1/light/turnlamp", turnLamp).Methods("PUT")
+	router.HandleFunc("/api/v1/climate", climateHandler).Methods("GET")
 	log.Fatal(http.ListenAndServe(":8000", router))
+}
+
+func climateHandler(w http.ResponseWriter, r *http.Request) {
+	temperature, humidity, retried, err := dht.ReadDHTxxWithRetry(dht.DHT11, 4, true, 10)
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Printf("Temperature = %v*C, Humidity = %v%% (retried %d times)\n",
+		temperature, humidity, retried)
 }
 
 func getLamp(w http.ResponseWriter, r *http.Request) {
